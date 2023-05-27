1bc63b855a4ad8c1c09ef0dbe589259767ff7443
DRQSuperior <74627000+DRQSuperior@users.noreply.github.com>
2023-05-27 00:08:54 -0500

    Update main.py
    Formats info to json and some other checks

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import random
 import urllib.parse
 import urllib.request
 import socket
+from bs4 import BeautifulSoup
 
 with open('proxies.txt', 'r') as file:
     proxies = [line.strip() for line in file.readlines()]
@@ -31,10 +32,34 @@ while proxies:
 
     try:
         response = opener.open(request, timeout=5)
-        print(response.read().decode('utf-8'))
-        break
+        soup = BeautifulSoup(response.read(), 'html.parser')
+
+        if 'You can only resolve 1 user every 30 minutes.' in soup.text:
+            print(f'Proxy {proxy} is on cooldown. Retrying with a new proxy...')
+            proxies.remove(proxy)
+            continue
+
+        try:
+            info = {
+                'Gamertag': gamertag,
+                'IP Address': soup.find(string='IP Address').find_next('td').get_text(strip=True),
+                'Internet Service Provider': soup.find(string='Internet Service Provider').find_next('td').get_text(strip=True),
+                'Latitude': soup.find(string='Latitude').find_next('td').get_text(strip=True),
+                'Longitude': soup.find(string='Longitude').find_next('td').get_text(strip=True),
+                'Postal Code': soup.find(string='Postal Code').find_next('td').get_text(strip=True),
+                'City': soup.find(string='City').find_next('td').get_text(strip=True),
+                'Country': soup.find(string='Country').find_next('td').get_text(strip=True),
+                'Continent': soup.find(string='Continent').find_next('td').get_text(strip=True),
+                'Region': soup.find(string='Region').find_next('td').get_text(strip=True),
+                'District': soup.find(string='District').find_next('td').get_text(strip=True),
+                'Timezone': soup.find(string='Timezone').find_next('td').get_text(strip=True),
+                'Connection Type': soup.find(string='Connection Type').find_next('td').get_text(strip=True),
+                'Currency': soup.find(string='Currency').find_next('td').get_text(strip=True)
+            }
+            print(info)
+            break
+        except AttributeError:
+            print('No information found for this gamertag.')
+            break
     except (urllib.error.URLError, socket.timeout):
-        print(f'Request timed out with proxy: {proxy}. Retrying with a new proxy...')
-        proxies.remove(proxy)
-else:
-    print('No more proxies to try. Request failed.')
+        print(f'Request timed out with proxy: {proxy}. Retrying')
